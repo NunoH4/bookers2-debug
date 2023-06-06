c7eca0d776722c2fe3ef6e531f0e6aa7fc80a183
NunoH4 <132256457+NunoH4@users.noreply.github.com>
2023-06-06 07:17:42 +0000

    [Add]Book詳細ページにコメント数表示

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,20 +5,18 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    
+    # 以下はコメント機能を非同期通信にしたため不必要
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    
+    # 以下はコメント機能を非同期通信にしたため不必要
+    # redirect_back(fallback_location: root_path)
   end
   
-  # def destroy
-  #   comment = BookComment.find(params[:id])
-  #   comment.destroy
-  #   redirect_back(fallback_location: root_path)
-  # end
-
   private
 
   def book_comment_params
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,17 +1,22 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+
+    # 以下はいいね機能を非同期通信にしたため不必要
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+
+    # 以下はいいね機能を非同期通信にしたため不必要
+    # redirect_back(fallback_location: root_path)
   end
 
 end
